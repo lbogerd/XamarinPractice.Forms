ee1183e46a941cb9153ad402758facebec5e352a
Lucas Bogerd <l.bogerd@protonmail.com>
2020-04-06 18:55:37 +0200

    Add project files.

---
+++ XamarinPractice/XamarinPractice/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinPractice
+{
+	// Learn more about making custom code visible in the Xamarin.Forms previewer
+	// by visiting https://aka.ms/xamarinforms-previewer
+	[DesignTimeVisible(false)]
+	public partial class MainPage : ContentPage
+	{
+		bool isTaskRunning = true;
+
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		void OnButtonClicked(object sender, EventArgs args)
+		{
+			isTaskRunning = !isTaskRunning;
+			isLoading.IsRunning = isTaskRunning;
+			mainLabel.BackgroundColor = Color.Red;
+		}
+	}
+}
